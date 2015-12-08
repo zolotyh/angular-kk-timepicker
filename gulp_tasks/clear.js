ac03aafc77650b1e57373e3592f0c6f8043b3212
Alex Zolotyh <aazolotyh@yandex.ru>
2015-12-08 07:11:40 +0300

    Update 2015-12-08T04:11:37.172Z

--- gulp_tasks/clear.js
+++
@@ -1,7 +0,0 @@
-var del = require('del');
-
-module.exports = function (gulp, plugins, config) {
-  gulp.task('clear', function(){
-    return del([config.dist]);
-  });
-};
--- gulp_tasks/reload.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (gulp, plugins, config) {
-  gulp.task('reload', function(){
-    config.browserSync.reload();
-  });
-};
